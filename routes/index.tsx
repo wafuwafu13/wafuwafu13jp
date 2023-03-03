0ea095122df010f44a0bd969b630335ada242cd9
wafuwafu13 <mariobaske@i.softbank.jp>
2023-03-03 20:57:28 +0900

    feat: embed youtube

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,6 +10,16 @@ export default function Home() {
         <p class="my-6">
           wafuwafu13 page
         </p>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/44XO1a5gjl8"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        >
+        </iframe>
       </div>
     </>
   );
